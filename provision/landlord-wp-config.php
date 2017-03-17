9485ee7f48715d46d88f7e1dc0a49e6fe1b57dcb
Aaron Graham <aegraham@gmail.com>
2017-03-17 15:35:33 -0500

    Update landlord's wp-config to match presentation (and remove extra lines we don't need)

--- provision/landlord-wp-config.php
+++ provision/landlord-wp-config.php
@@ -20,12 +20,3 @@
 
 // NOTE: this WordPress install is configured for multitenancy
 include_once( $_SERVER['DOCUMENT_ROOT'] . '/wp-config.php' );
-
-/* That's all, stop editing! Happy blogging. */
-
-/** Absolute path to the WordPress directory. */
-if ( ! defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
-
-/** Sets up WordPress vars and included files. */
-require_once ABSPATH . 'wp-settings.php';
