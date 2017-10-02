da7d5d6d80a675173a95652d51a5ddd98d2341f6
Zachary Flauaus <zflauaus@wustl.edu>
2017-10-02 16:26:15 -0500

    Added mu-plugins symlink creation.

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -41,6 +41,7 @@ if [[ ! -d "${VVV_PATH_TO_SITE}/public_html" ]]; then
 
   cd ${VVV_PATH_TO_SITE}/public_html/wp-content
   # Symlink the plugins and themes to the deafult install's plugins and themes
+  ln -s ../../../landlord/wp-content/mu-plugins mu-plugins
   ln -s ../../../landlord/wp-content/plugins plugins
   ln -s ../../../landlord/wp-content/themes themes
 
